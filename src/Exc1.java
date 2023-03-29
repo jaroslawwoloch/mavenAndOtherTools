20b949dcbbf926d1428617a7db318c02375f2469
Wołoch Jarosław (FIRMA ZEWNĘTRZNA ) <altkom_jwoloch@pzu.pl>
2023-03-29 14:05:49 +0200

    init statndard Project

---
+++ src/Exc1.java
@@ -0,0 +1,10 @@
+import org.apache.log4j.Logger;
+
+public class Exc1 {
+
+    private static final Logger LOG = Logger.getLogger(Exc1.class);
+
+    public static void main(String[] args) {
+        LOG.info("hello");
+    }
+}
